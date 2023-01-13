9229fb234f6eda8ac5dd9cdd6da6a32ad6ad7bae
Balaji Haridasan <balajiharidasan@gmail.com>
2023-01-13 23:49:19 +0530

    updated

--- setup.py
+++ setup.py
@@ -12,16 +12,15 @@ REQUIREMENT_FILE_NAME="requirements.txt"
 HYPHEN_E_DOT = "-e ."  # to run the packages in the requirements.txt
 
 
-def get_requirements_list() -> List[str]:
+def get_requirements_list() -> List[str]: # return output of the function as list
     """
     Description: This function is going to return list of requirement
     mention in requirements.txt file
-    return This function is going to return a list which contain name
-    of libraries mentioned in requirements.txt file
+
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_list = requirement_file.readlines()
-        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        requirement_list = requirement_file.readlines() # readlines in the requirments.txt file
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list] # 
         if HYPHEN_E_DOT in requirement_list:
             requirement_list.remove(HYPHEN_E_DOT)
         return requirement_list
